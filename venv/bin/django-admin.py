09bd200f9507fdb6884b1e2cf956c12220fb8f31
sh7078 <shgpta30@gmail.com>
2020-03-31 19:49:50 +0530

    Change fully in frontend

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/madhav/Desktop/robohack1.o/Food-Ordering-System/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
