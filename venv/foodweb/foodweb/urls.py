c1c2610a36ea7200abd56dee5b8d235c63d4e58a
sh7078 <shgpta30@gmail.com>
2020-03-31 11:27:03 +0530

    adding an notification feature

--- venv/foodweb/foodweb/urls.py
+++ venv/foodweb/foodweb/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webapp.urls')),
+    url(r'^webpush/', include('webpush.urls')),
 ]
--- venv/foodweb/webapp/views.py
+++ venv/foodweb/webapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from webpush import send_user_notification
 
+payload = {"head": "Welcome!", "body": "Hello World"}
+
+#ye wala part jb lgaio jb notification bhejna ho
+send_user_notification(user=user, payload=payload, ttl=1000)
 # Create your views here.
 def index(request):
     return render(request,'webapp/index.html',{})
